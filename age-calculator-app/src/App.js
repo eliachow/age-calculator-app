83d1d4b055b2ddb148aa0524031e4ee7e3c91bca
eliachow <eliacchow@gmail.com>
2024-06-16 11:31:14 +0100

    setup initial structure

---
+++ age-calculator-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Input from './components/Input';
+import Output from './components/Output';
+
+
+export default function App() {
+  return (
+    <div className="App">
+      <Input />
+      <Output />
+    </div>
+  );
+}
---
+++ age-calculator-app/src/components/Input.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Input() {
+  return (
+    <div>
+        Day
+        DD
+
+        Month
+        MM
+
+        Year
+        YYYY
+    </div>
+  )
+}
+
