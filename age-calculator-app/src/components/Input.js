9e540dfab4843d0ea783a80ed62850db653b5260
eliachow <eliacchow@gmail.com>
2024-06-22 13:50:38 +0100

    align warn msg w/ input

--- age-calculator-app/src/components/Input.js
+++ age-calculator-app/src/components/Input.js
@@ -4,6 +4,9 @@ export default function Input() {
   const [day, setDay] = useState('');
   const [month, setMonth] = useState('');
   const [year, setYear] = useState('');
+  const [dWarn, setDWarn] = useState('');
+  const [mWarn, setMWarn] = useState('');
+  const [yWarn, setYWarn] = useState('');
   const [exMo, setExMo] = useState('');
 
   function handleDayChange(e) { setDay(e.target.value) }
@@ -22,18 +25,49 @@ export default function Input() {
     document.getElementById('exceed').classList.remove('on');
 
     // validate day
-    if(day.trim() === '' || day.trim() === null || isNaN(day.trim()) || day < 1 || day > 31) {
+    if(day.trim() === '' || day.trim() === null) {
+      setDWarn('Must not be empty')
+      document.getElementById('dd').classList.add('on');
+      isValid = false;
+    } else if(isNaN(day.trim())) {
+      setDWarn('Must be a number')
+      document.getElementById('dd').classList.add('on');
+      isValid = false;
+    } else if (day < 1 || day > 31) {
+      setDWarn('Must a day within the month')
       document.getElementById('dd').classList.add('on');
       isValid = false;
     }
     // validate month
-    if(month.trim() === '' || month.trim() === null || isNaN(month.trim()) || month < 1 || month > 12) {
+    if(month.trim() === '' || month.trim() === null) {
+      setMWarn('Must not be empty')
+      document.getElementById('mo').classList.add('on');
+      isValid = false;
+    } else if (isNaN(month.trim())) {
+      setMWarn('Must be a number')
+      document.getElementById('mo').classList.add('on');
+      isValid = false;
+    } else if ( month < 1 || month > 12) {
+      setMWarn('Must be between 1-12')
       document.getElementById('mo').classList.add('on');
       isValid = false;
     }
     // validate year
     const currentYear = new Date().getFullYear();
-    if(year.trim() === '' || year.trim() === null || isNaN(year.trim()) || year > currentYear || year.length !== 4) {
+    if(year.trim() === '' || year.trim() === null) {
+      setYWarn('Must not be empty');
+      document.getElementById('yr').classList.add('on');
+      isValid = false;
+    } else if (isNaN(year.trim())) {
+      setYWarn('Must be a number');
+      document.getElementById('yr').classList.add('on');
+      isValid = false;
+    } else if (year > currentYear) {
+      setYWarn('Must be in the past');
+      document.getElementById('yr').classList.add('on');
+      isValid = false;
+    } else if (year.length !== 4) {
+      setYWarn('Must be 4 characters');
       document.getElementById('yr').classList.add('on');
       isValid = false;
     }
@@ -69,17 +103,17 @@ export default function Input() {
           <div className="flex c">
             <label>Day</label>
             <input type="text" placeholder="DD" value={day} onChange={handleDayChange}></input>
-            <div className="warn" id="dd">Must be a valid day</div>
+            <div className="warn" id="dd">{dWarn}</div>
           </div>
           <div className="flex c" >
             <label>Month</label>
             <input type="text" placeholder="MM" value={month} onChange={handleMonthChange}></input>
-            <div className="warn" id="mo">Must be a valid month</div>
+            <div className="warn" id="mo">{mWarn}</div>
           </div>
           <div className="flex c">
             <label>Year</label>
             <input type="text" placeholder="YYYY" value={year} onChange={handleYearChange}></input>
-            <div className="warn" id="yr">Must be in the past</div>
+            <div className="warn" id="yr">{yWarn}</div>
           </div>
           <div className="warn" id="exceed">There are not {day} days in {exMo}</div>
           <button className="btn-submit" type="submit" onClick={handleSubmit}></button>
